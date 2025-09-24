f0841506b7b287f9d602c6529eb27f5fa5ed5f00
namnguyen <namhoanghainguyen@gmail.com>
2025-09-24 15:44:29 -0600

    Starting new project

---
+++ src/main/java/com/example/TodoListAPILearning/TodoListApiLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.TodoListAPILearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApiLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApiLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TodoListAPILearning/TodoListApiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TodoListAPILearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
